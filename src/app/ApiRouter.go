6f676249abe9fa177a43a418f67293d8aeb7b3fe
Didier Page <didier.page@gmail.com>
2021-09-14 18:16:55 +0200

    Extract router config from main

---
+++ src/app/ApiRouter.go
@@ -0,0 +1,9 @@
+package app
+
+import "github.com/gin-gonic/gin"
+
+func initApiRouter(router *gin.Engine) *gin.Engine  {
+
+
+	return router
+}
---
+++ src/app/GuiRouter.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"main/src/internal/controllers"
+)
+
+func initGuiRouter(router *gin.Engine) *gin.Engine {
+
+	router.GET("/", controllers.Render)
+	router.GET("/accounts/:accountId", controllers.GetAccount)
+
+	router.GET("/uris",controllers.GetUri)
+	router.GET("/uris/:uuid",controllers.GetUriByUUID)
+
+	router.GET("/uris/create",controllers.RenderUriForm)
+	router.POST("/uris/create",controllers.CreateUri)
+
+
+	router.GET("/tokens",controllers.GetTokens)
+	router.GET("/tokens/create",controllers.RenderTokenForm)
+	router.POST("/tokens/create",controllers.CreateToken)
+	router.GET("/tokens/:id/delete",controllers.DeleteToken)
+
+
+	return router
+
+}
