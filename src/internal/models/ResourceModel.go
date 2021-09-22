444dab63f28ff5dae6d9405dbdbb08027a4daaaa
Didier Page <didier.page@gmail.com>
2021-09-22 13:45:32 +0200

    Update resource form

--- src/internal/models/ResourceModel.go
+++ src/internal/models/ResourceModel.go
@@ -1,11 +1,14 @@
 package models
 
+import "time"
+
 type Resource struct {
 	Uuid    string `gorm:"primary_key"`
-	Content string
+	Document string
+	AccountId string `gorm:"references:GoTrueId"`
+	// Account Account `gorm:"references:GoTrueId"`
 
-	AccountId string
-	Account Account `gorm:"references:GoTrueId"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
-
 
